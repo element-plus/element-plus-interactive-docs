add0fa82bcde8ec70e9f392e40f28eca87c86707
joypeng <70848683+FliPPeDround@users.noreply.github.com>
2022-10-15 16:21:56 +0800

    feat: button component (#27)
    * feat: button component
    
    * fix: build package.json error
    
    * fix: type default and size default conflicts

--- src/components/Basic/button.story.vue
+++ src/components/Basic/button.story.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { logEvent } from 'histoire/client'
-import type { ButtonProps } from 'element-plus'
+import {
+  ArrowLeft,
+  ArrowRight,
+} from '@element-plus/icons-vue'
 
-const buttonType = ref<ButtonProps['type']>('success')
-const buttonTypeList = [
+const typeList = [
   {
     label: 'default',
-    value: 'default',
+    value: '',
   },
   {
     label: 'primary',
@@ -30,38 +32,129 @@ const buttonTypeList = [
     value: 'info',
   },
 ]
+
+const sizeList = [
+  {
+    label: 'large',
+    value: 'large',
+  },
+  {
+    label: 'default',
+    value: 'default',
+  },
+  {
+    label: 'small',
+    value: 'small',
+  },
+]
+
+const basicButtonData = ref({
+  text: 'Button',
+  type: '',
+  typeList,
+  size: '',
+  sizeList,
+  plain: false,
+  round: false,
+  circle: false,
+  loading: false,
+  disabled: false,
+})
+
+const colorButtonData = ref({
+  text: 'Custom Color Button',
+  color: '#626aef',
+  plain: false,
+  disabled: false,
+})
+const buttonGroupData = ref({
+  type: '',
+  typeList,
+  size: '',
+  sizeList,
+})
 </script>
 
 <template>
   <Story title="Basic/Button">
-    <Variant title="Basic usage">
-      <el-button :type="buttonType" @click="logEvent('click', $event)">
-        {{ buttonType }}
+    <Variant title="Basic usage" auto-props-disabled>
+      <el-button
+        :type="basicButtonData.type"
+        :size="basicButtonData.size"
+        :plain="basicButtonData.plain"
+        :round="basicButtonData.round"
+        :disabled="basicButtonData.disabled"
+        :loading="basicButtonData.loading"
+        @click="logEvent('click', $event)"
+      >
+        {{ basicButtonData.text }}
       </el-button>
-
       <template #controls>
-        <HstRadio v-model="buttonType" title="type" :option="buttonTypeList" />
+        <HstText v-model="basicButtonData.text" title="Text" />
+        <HstCheckbox v-model="basicButtonData.plain" title="Plain" />
+        <HstCheckbox v-model="basicButtonData.round" title="Round" />
+        <HstCheckbox v-model="basicButtonData.disabled" title="Disabled" />
+        <HstCheckbox v-model="basicButtonData.loading" title="Loading" />
+
+        <HstRadio
+          v-model="basicButtonData.type"
+          title="Button Type"
+          :options="basicButtonData.typeList"
+        />
+        <HstRadio
+          v-model="basicButtonData.size"
+          title="Size"
+          :options="basicButtonData.sizeList"
+        />
       </template>
     </Variant>
-    <Variant title="Disabled Button">
-      <el-button disabled>
-        Default
+    <Variant title="Custom Color" auto-props-disabled>
+      <el-button
+        :plain="colorButtonData.plain"
+        :disabled="colorButtonData.disabled"
+        :color="colorButtonData.color"
+        @click="logEvent('click', $event)"
+      >
+        {{ colorButtonData.text }}
       </el-button>
-      <el-button type="primary" disabled>
-        Primary
-      </el-button>
-      <el-button type="success" disabled>
-        Success
-      </el-button>
-      <el-button type="info" disabled>
-        Info
-      </el-button>
-      <el-button type="warning" disabled>
-        Warning
-      </el-button>
-      <el-button type="danger" disabled>
-        Danger
-      </el-button>
+      <template #controls>
+        <HstText v-model="colorButtonData.text" title="Text" />
+        <span style="padding: 0 8px; font-size: 14px; margin-right: 68px;">
+          Color:
+        </span>
+        <el-color-picker v-model="colorButtonData.color" size="small" />
+        <HstCheckbox v-model="colorButtonData.plain" title="Plain" />
+        <HstCheckbox v-model="colorButtonData.disabled" title="Disabled" />
+      </template>
+    </Variant>
+
+    <Variant title="Button Group" auto-props-disabled>
+      <el-button-group
+        :size="buttonGroupData.size"
+        :type="buttonGroupData.type"
+      >
+        <el-button :icon="ArrowLeft">
+          Previous Page
+        </el-button>
+        <el-button>
+          Next Page
+          <el-icon class="el-icon--right">
+            <arrow-right />
+          </el-icon>
+        </el-button>
+      </el-button-group>
+      <template #controls>
+        <HstRadio
+          v-model="buttonGroupData.type"
+          title="Button Type"
+          :options="buttonGroupData.typeList"
+        />
+        <HstRadio
+          v-model="buttonGroupData.size"
+          title="Size"
+          :options="buttonGroupData.sizeList"
+        />
+      </template>
     </Variant>
   </Story>
 </template>
