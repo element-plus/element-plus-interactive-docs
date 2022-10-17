3b6104ec89ffe707b4380d724a49f99ecd3e9b03
BTB <1498118157@qq.com>
2022-10-17 14:52:02 +0800

    feat: element plus dark mode follow histoire (#50)

--- src/histoire.setup.ts
+++ src/histoire.setup.ts
@@ -2,6 +2,7 @@ import { defineSetupVue3 } from '@histoire/plugin-vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import './dark-mode'
 
 export const setupVue3 = defineSetupVue3(({ app }) => {
   app.use(ElementPlus)
