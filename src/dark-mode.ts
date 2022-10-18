c424c3ed1f837f7d98d0cbd758a659d9e61162fe
ZhangZhiChao <54631354+Alkaidcc@users.noreply.github.com>
2022-10-18 09:49:25 +0800

    fix: dark mode #50 (#53)
    * fix: dark mode #50
    
    * fix: async `setDarkMode`
    
    Co-authored-by: BTBMan <1498118157@qq.com>

--- src/dark-mode.ts
+++ src/dark-mode.ts
@@ -1,14 +1,16 @@
 import { useLocalStorage, useMutationObserver } from '@vueuse/core'
 
 const setDarkMode = () => {
-  const vueuseColorScheme = useLocalStorage('vueuse-color-scheme', 'auto')
-  const colorMode = vueuseColorScheme.value === 'auto' ? 'dark' : ''
-  const classList = Array.from(document.documentElement.classList)
+  setTimeout(() => {
+    const vueuseColorScheme = useLocalStorage('vueuse-color-scheme', 'auto')
+    const colorMode = vueuseColorScheme.value === 'auto' ? 'dark' : ''
+    const classList = Array.from(document.documentElement.classList)
 
-  if (classList.length && !classList.includes(colorMode)) {
-    const classNames = colorMode === 'dark' ? 'htw-dark dark' : ''
-    document.documentElement.setAttribute('class', classNames)
-  }
+    if (classList.length && !classList.includes(colorMode)) {
+      const classNames = colorMode === 'dark' ? 'htw-dark dark' : ''
+      document.documentElement.setAttribute('class', classNames)
+    }
+  }, 0)
 }
 setDarkMode()
 
