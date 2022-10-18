d20d496bd792a3b2c8c67209ef9afd57e8fc38d4
vkm0303 <1947055799@qq.com>
2022-10-18 17:23:39 +0800

    feat: put the controls of Animation in Controls.

--- src/components/Data/skeleton.story.vue
+++ src/components/Data/skeleton.story.vue
@@ -68,13 +68,10 @@ onMounted(() => {
       </template>
     </Variant>
     <Variant title="Animation">
-      <el-space direction="vertical" alignment="flex-start">
-        <div>
-          <label style="margin-right: 16px">Switch Animated</label>
-          <el-switch v-model="animated" />
-        </div>
-        <el-skeleton style="width: 240px" :rows="5" :animated="animated" />
-      </el-space>
+      <el-skeleton :rows="5" :animated="animated" />
+      <template #controls>
+        <HstCheckbox v-model="animated" title="animated" />
+      </template>
     </Variant>
     <Variant title="Customized Template">
       <el-skeleton style="width: 240px">
@@ -97,43 +94,40 @@ onMounted(() => {
       </el-skeleton>
     </Variant>
     <Variant title="Loading state">
-      <el-space direction="vertical" alignment="flex-start">
-        <div>
-          <label style="margin-right: 16px">Switch Loading</label>
-          <el-switch v-model="loading[0]" />
-        </div>
-        <el-skeleton style="width: 240px" :loading="loading[0]" animated>
-          <template #template>
-            <el-skeleton-item variant="image" style="width: 240px; height: 240px" />
+      <el-skeleton style="width: 240px" :loading="loading[0]" animated>
+        <template #template>
+          <el-skeleton-item variant="image" style="width: 240px; height: 240px" />
+          <div style="padding: 14px">
+            <el-skeleton-item variant="h3" style="width: 50%" />
+            <div class="skeleton-item-wrapper">
+              <el-skeleton-item variant="text" style="margin-right: 16px" />
+              <el-skeleton-item variant="text" style="width: 30%" />
+            </div>
+          </div>
+        </template>
+        <template #default>
+          <el-card :body-style="{ padding: '0px', marginBottom: '1px' }">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            >
             <div style="padding: 14px">
-              <el-skeleton-item variant="h3" style="width: 50%" />
-              <div class="skeleton-item-wrapper">
-                <el-skeleton-item variant="text" style="margin-right: 16px" />
-                <el-skeleton-item variant="text" style="width: 30%" />
+              <span>Delicious hamburger</span>
+              <div class="bottom card-header">
+                <div class="time">
+                  {{ currentDate }}
+                </div>
+                <el-button text class="button">
+                  Operation button
+                </el-button>
               </div>
             </div>
-          </template>
-          <template #default>
-            <el-card :body-style="{ padding: '0px', marginBottom: '1px' }">
-              <img
-                src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                class="image"
-              >
-              <div style="padding: 14px">
-                <span>Delicious hamburger</span>
-                <div class="bottom card-header">
-                  <div class="time">
-                    {{ currentDate }}
-                  </div>
-                  <el-button text class="button">
-                    Operation button
-                  </el-button>
-                </div>
-              </div>
-            </el-card>
-          </template>
-        </el-skeleton>
-      </el-space>
+          </el-card>
+        </template>
+      </el-skeleton>
+      <template #controls>
+        <HstCheckbox v-model="loading[0]" title="loading" />
+      </template>
     </Variant>
     <Variant title="Rendering a list of data">
       <el-space direction="vertical" alignment="flex-start">
