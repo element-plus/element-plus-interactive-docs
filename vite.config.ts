aca8974102f5d199d2a9cbf74bbc24fbb8f0d115
flippedround <734243792@qq.com>
2022-10-25 21:26:59 +0800

    chore: add auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        '@vueuse/core',
+      ],
+      dts: true,
+      vueTemplate: true,
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+
+  ],
 })
