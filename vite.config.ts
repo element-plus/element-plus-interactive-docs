d669544cb2abd0d96feff64c14261cba90e340cb
flippedround <734243792@qq.com>
2022-10-26 15:24:59 +0800

    feat: improve-button

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       '@': `${path.resolve(__dirname, 'src')}/`,
       '@components': `${path.resolve(__dirname, 'src/components')}/`,
       '@constants': `${path.resolve(__dirname, 'src/constants')}/`,
+      '@utils': `${path.resolve(__dirname, 'src/utils')}/`,
     },
   },
   plugins: [
