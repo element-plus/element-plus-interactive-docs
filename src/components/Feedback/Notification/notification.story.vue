b3c311295154b0d5444214cc9e1ff60df1507a28
flippedround <734243792@qq.com>
2022-11-06 10:33:48 +0800

    fix: typo

--- src/components/Feedback/Notification/notification.story.vue
+++ src/components/Feedback/Notification/notification.story.vue
@@ -2,8 +2,8 @@
 import type { NotificationProps } from 'element-plus'
 import { positionList, typeList } from './constants'
 
-// Basic-Nofitication
-const basicNofiticationData = reactive({
+// Basic-Notification
+const basicNotificationData = reactive({
   title: 'title',
   message: 'message',
   type: ref<NotificationProps['type']>(''),
@@ -11,87 +11,87 @@ const basicNofiticationData = reactive({
 })
 const basicNotificationOpen = () => {
   ElNotification({
-    title: basicNofiticationData.title,
-    message: basicNofiticationData.message,
-    type: basicNofiticationData.type,
+    title: basicNotificationData.title,
+    message: basicNotificationData.message,
+    type: basicNotificationData.type,
   })
 }
 const basicSource = computed(() => {
   return `<script setup lang="ts">
   const basicNotificationOpen = () => {
     ElNotification({
-      title: '${basicNofiticationData.title}',
-      message: '${basicNofiticationData.message}',
-      ${basicNofiticationData.type !== '' ? `type: '${basicNofiticationData.type}',` : ''}
+      title: '${basicNotificationData.title}',
+      message: '${basicNotificationData.message}',
+      ${basicNotificationData.type !== '' ? `type: '${basicNotificationData.type}',` : ''}
     })
   }
 <\/script>
 
 <template>
   <el-button @click="basicNotificationOpen">
-    basicNofitication
+    basicNotification
   </el-button> 
 </template>`
 })
 
-// CustomClose-Nofitication
-const customCloseNofiticationData = reactive({
+// CustomClose-Notification
+const customCloseNotificationData = reactive({
   title: 'title',
   message: 'message',
   duration: ref<NotificationProps['duration']>(4500),
   showClose: ref<NotificationProps['showClose']>(true),
 })
-const customCloseNofiticationOpen = () => {
+const customCloseNotificationOpen = () => {
   ElNotification({
-    title: customCloseNofiticationData.title,
-    message: customCloseNofiticationData.message,
-    duration: customCloseNofiticationData.duration,
-    showClose: customCloseNofiticationData.showClose,
+    title: customCloseNotificationData.title,
+    message: customCloseNotificationData.message,
+    duration: customCloseNotificationData.duration,
+    showClose: customCloseNotificationData.showClose,
   })
 }
 const customCloseSource = computed(() => {
   return `<script setup lang="ts">
-  const customCloseNofiticationOpen = () => {
+  const customCloseNotificationOpen = () => {
     ElNotification({
-      title: '${customCloseNofiticationData.title}',
-      message: '${customCloseNofiticationData.message}',
-      duration: '${customCloseNofiticationData.duration}',
-      showClose: '${customCloseNofiticationData.showClose}',
+      title: '${customCloseNotificationData.title}',
+      message: '${customCloseNotificationData.message}',
+      duration: '${customCloseNotificationData.duration}',
+      showClose: '${customCloseNotificationData.showClose}',
     })
   }
 <\/script>
 
 <template>
-  <el-button @click="customCloseNofiticationOpen">
-    customCloseNofitication
+  <el-button @click="customCloseNotificationOpen">
+    customCloseNotification
   </el-button> 
 </template>`
 })
 
-// Position-Nofitication
-const positionNofiticationData = reactive({
+// Position-Notification
+const positionNotificationData = reactive({
   title: 'title',
   message: 'message',
   position: ref<NotificationProps['position']>('top-right'),
   offset: ref<NotificationProps['offset']>(0),
   positionList,
 })
-const positionNofiticationOpen = () => {
+const positionNotificationOpen = () => {
   ElNotification({
-    title: positionNofiticationData.title,
-    message: positionNofiticationData.message,
-    position: positionNofiticationData.position,
-    offset: positionNofiticationData.offset,
+    title: positionNotificationData.title,
+    message: positionNotificationData.message,
+    position: positionNotificationData.position,
+    offset: positionNotificationData.offset,
   })
 }
 const positionSource = computed(() => {
   return `<script setup lang="ts">
-  const positionNofiticationOpen = () => {
+  const positionNotificationOpen = () => {
     ElNotification({
-      title: '${positionNofiticationData.title}',
-      message: '${positionNofiticationData.message}',
-      position: '${positionNofiticationData.position}',
-      offset: '${positionNofiticationData.offset}',
+      title: '${positionNotificationData.title}',
+      message: '${positionNotificationData.message}',
+      position: '${positionNotificationData.position}',
+      offset: '${positionNotificationData.offset}',
     })
   }
 <\/script>
@@ -103,33 +103,33 @@ const positionSource = computed(() => {
 </template>`
 })
 
-// htmlMessage-Nofitication
-const htmlMessageNofiticationData = reactive({
+// htmlMessage-Notification
+const htmlMessageNotificationData = reactive({
   title: 'title',
   message: 'message',
   dangerouslyUseHTMLString: ref<NotificationProps['dangerouslyUseHTMLString']>(true),
 })
-const htmlMessageNofiticationOpen = () => {
+const htmlMessageNotificationOpen = () => {
   ElNotification({
-    title: htmlMessageNofiticationData.title,
-    dangerouslyUseHTMLString: htmlMessageNofiticationData.dangerouslyUseHTMLString,
-    message: htmlMessageNofiticationData.message,
+    title: htmlMessageNotificationData.title,
+    dangerouslyUseHTMLString: htmlMessageNotificationData.dangerouslyUseHTMLString,
+    message: htmlMessageNotificationData.message,
   })
 }
-const htmlMessageNofiticationSource = computed(() => {
+const htmlMessageNotificationSource = computed(() => {
   return `<script setup lang="ts">
-  const htmlMessageNofiticationOpen = () => {
+  const htmlMessageNotificationOpen = () => {
     ElNotification({
-      title: '${htmlMessageNofiticationData.title}',
-      message: '${htmlMessageNofiticationData.message}',
-      dangerouslyUseHTMLString: '${htmlMessageNofiticationData.dangerouslyUseHTMLString}',
+      title: '${htmlMessageNotificationData.title}',
+      message: '${htmlMessageNotificationData.message}',
+      dangerouslyUseHTMLString: '${htmlMessageNotificationData.dangerouslyUseHTMLString}',
     })
   }
 <\/script>
 
 <template>
-  <el-button @click="htmlMessageNofiticationOpen">
-    htmlMessageNofitication
+  <el-button @click="htmlMessageNotificationOpen">
+    htmlMessageNotification
   </el-button> 
 </template>`
 })
@@ -139,51 +139,51 @@ const htmlMessageNofiticationSource = computed(() => {
   <Story title="Feedback/Notification" icon="ri:notification-badge-line">
     <Variant title="Basic Usage" :source="basicSource">
       <el-button @click="basicNotificationOpen">
-        basicNofitication
+        basicNotification
       </el-button>
       <template #controls>
-        <HstText v-model="basicNofiticationData.title" title="Title" />
-        <HstText v-model="basicNofiticationData.message" title="Message" />
+        <HstText v-model="basicNotificationData.title" title="Title" />
+        <HstText v-model="basicNotificationData.message" title="Message" />
         <HstRadio
-          v-model="basicNofiticationData.type"
+          v-model="basicNotificationData.type"
           title="Type"
-          :options="basicNofiticationData.typeList"
+          :options="basicNotificationData.typeList"
         />
       </template>
     </Variant>
     <Variant title="Custom Show" :source="customCloseSource" icon="mdi:close-circle-multiple-outline">
-      <el-button @click="customCloseNofiticationOpen">
-        customCloseNofitication
+      <el-button @click="customCloseNotificationOpen">
+        customCloseNotification
       </el-button>
       <template #controls>
-        <HstText v-model="customCloseNofiticationData.title" title="Title" />
-        <HstText v-model="customCloseNofiticationData.message" title="Message" />
-        <HstNumber v-model="customCloseNofiticationData.duration" title="Duration" />
-        <HstCheckbox v-model="customCloseNofiticationData.showClose" title="ShowClose" />
+        <HstText v-model="customCloseNotificationData.title" title="Title" />
+        <HstText v-model="customCloseNotificationData.message" title="Message" />
+        <HstNumber v-model="customCloseNotificationData.duration" title="Duration" />
+        <HstCheckbox v-model="customCloseNotificationData.showClose" title="ShowClose" />
       </template>
     </Variant>
     <Variant title="Position" :source="positionSource" icon="fluent:position-to-back-24-regular">
-      <el-button @click="positionNofiticationOpen">
-        positionNofitication
+      <el-button @click="positionNotificationOpen">
+        positionNotification
       </el-button>
       <template #controls>
-        <HstText v-model="positionNofiticationData.title" title="Title" />
-        <HstText v-model="positionNofiticationData.message" title="Message" />
-        <HstNumber v-model="positionNofiticationData.offset" title="Offset" />
+        <HstText v-model="positionNotificationData.title" title="Title" />
+        <HstText v-model="positionNotificationData.message" title="Message" />
+        <HstNumber v-model="positionNotificationData.offset" title="Offset" />
         <HstRadio
-          v-model="positionNofiticationData.position"
+          v-model="positionNotificationData.position"
           title="Position"
-          :options="positionNofiticationData.positionList"
+          :options="positionNotificationData.positionList"
         />
       </template>
     </Variant>
-    <Variant title="HTML Message" :source="htmlMessageNofiticationSource" icon="bi:filetype-html">
-      <el-button @click="htmlMessageNofiticationOpen">
-        htmlMessageNofitication
+    <Variant title="HTML Message" :source="htmlMessageNotificationSource" icon="bi:filetype-html">
+      <el-button @click="htmlMessageNotificationOpen">
+        htmlMessageNotification
       </el-button>
       <template #controls>
-        <HstText v-model="htmlMessageNofiticationData.title" title="Title" />
-        <HstTextarea v-model="htmlMessageNofiticationData.message" title="Message" />
+        <HstText v-model="htmlMessageNotificationData.title" title="Title" />
+        <HstTextarea v-model="htmlMessageNotificationData.message" title="Message" />
       </template>
     </Variant>
   </Story>
