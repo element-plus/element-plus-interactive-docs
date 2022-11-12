5dcb35f27914fc51ad9fbf3445c7d28e7d80af81
FliPPeDround <70848683+FliPPeDround@users.noreply.github.com>
2022-11-12 15:22:46 +0800

    feat(TimeLine): init timeLine (#88)

---
+++ src/components/Data/Timeline/constants.ts
@@ -0,0 +1,21 @@
+export const placementList = [
+  {
+    label: 'top',
+    value: 'top',
+  },
+  {
+    label: 'bottom',
+    value: 'bottom',
+  },
+]
+
+export const sizeList = [
+  {
+    label: 'normal',
+    value: 'normal',
+  },
+  {
+    label: 'large',
+    value: 'large',
+  },
+]
