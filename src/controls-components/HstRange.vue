612e36d38131caee53d8acc13e08d72f3b6b4512
flippedround <734243792@qq.com>
2022-11-26 13:43:34 +0800

    feat: add range controle componets

---
+++ src/controls-components/HstRange.vue
@@ -0,0 +1,60 @@
+<script lang="ts">
+export default {
+  name: 'HstRange',
+  inheritAttrs: false,
+}
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string
+  modelValue: number[]
+  min?: number
+  max?: number
+}>()
+
+const emit = defineEmits({
+  'update:modelValue': (newValue: number[]) => true,
+})
+
+const RangeValue = computed({
+  get: () => props.modelValue,
+  set: (value) => {
+    emit('update:modelValue', value)
+  },
+})
+</script>
+
+<template>
+  <label class="container">
+    <span
+      class="title"
+    >
+      {{ title }}
+    </span>
+    <el-slider
+      v-model="RangeValue"
+      range
+      :max="max"
+      :min="min"
+    />
+  </label>
+</template>
+
+<style scoped>
+.container {
+  padding: 0.5rem;
+  gap: 0.5rem;
+  align-items: center;
+  flex-wrap: wrap;
+  cursor: pointer;
+  display: flex;
+}
+.title {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  flex-shrink: 0;
+  width: 7rem;
+}
+</style>
