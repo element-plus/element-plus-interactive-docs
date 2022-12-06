f4a3eb2a9ee2774a61e49ff8f6c138343feae2cd
FliPPeDround <70848683+FliPPeDround@users.noreply.github.com>
2022-12-06 22:12:26 +0800

    feat(Steps): init steps (#102)
    * feat(Steps): init steps

---
+++ src/components/Navigation/Steps/constants.ts
@@ -0,0 +1,22 @@
+export const stateOptions = [
+  {
+    label: 'process',
+    value: 'process',
+  },
+  {
+    label: 'wait',
+    value: 'wait',
+  },
+  {
+    label: 'finish',
+    value: 'finish',
+  },
+  {
+    label: 'error',
+    value: 'error',
+  },
+  {
+    label: 'success',
+    value: 'success',
+  },
+]
