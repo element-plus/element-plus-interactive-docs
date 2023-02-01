3ae079151b769a8b19a866535c053d57a4b3799e
flippedround <734243792@qq.com>
2023-02-01 14:32:26 +0800

    feat(Breadcrumb): init breadcrumb

---
+++ src/controls-components/HstDivider.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+  name: 'HstColor',
+  inheritAttrs: false,
+}
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+  content?: string
+}>()
+</script>
+
+<template>
+  <el-divider border-style="dotted" content-position="left">
+    <span>{{ props.content }}</span>
+  </el-divider>
+</template>
