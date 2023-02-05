bc16ee6c9ac9d07796304ad4d2fd9760937a773f
duer <957965700@qq.com>
2023-02-05 18:22:47 +0800

    feat(Infinite Scroll): add control Attribute(delay,distance)

--- src/components/Data/InfiniteScroll/infiniteScroll.story.vue
+++ src/components/Data/InfiniteScroll/infiniteScroll.story.vue
@@ -1,55 +1,41 @@
 <script setup lang="ts">
 // Basic-Usage
-const count = ref(2)
-const load = () => {
-  count.value += 2
-}
+const count = ref(7)
+const distance = ref(0)
+const delay = ref(200)
+const disabled = ref(false)
+const refresh = ref(true)
+const load = () => count.value += 2
 
-const basicSource = computed(() => {
-  return `<script setup lang="ts">
-  import { ref } from 'vue'
-  const count = ref(2)
-  const load = () => {
-    count.value += 2
-  }
-<\/script>
-<template>
-  <ul
-    v-infinite-scroll="load"
-    class="infinite-list"
-    style="overflow: auto"
-  >
-    <li v-for="i in count" :key="i" class="infinite-list-item">
-      {{ i }}
-    </li>
-  </ul>
-</template>`
-})
+debouncedWatch(delay, () => {
+  refresh.value = false
+  setTimeout(() => refresh.value = true, 0)
+}, { debounce: 500, maxWait: 1000 })
 
-// Disable Loading
-const count1 = ref(7)
-const disabled = ref(true)
-const load1 = () => {
-  count1.value += 2
-}
+debouncedWatch(distance, () => {
+  refresh.value = false
+  setTimeout(() => refresh.value = true, 0)
+}, { debounce: 500, maxWait: 1000 })
 
 const disableSource = computed(() => {
   return `<script setup lang="ts">
-  import { ref, computed } from 'vue'
-  const count1 = ref(7)
-  const disabled = ref(true)
-  const load1 = () => {
-    count1.value += 2
-  }
+  import { ref } from 'vue'
+  const count = ref(7)
+  const disabled = ref(false)
+  const delay = ref(200)
+  const distance = ref(0)
+  const load = () => count.value += 2
 <\/script>
 <template>
   <div class="infinite-list-wrapper" style="overflow: auto">
     <ul
-      v-infinite-scroll="load1"
+      v-infinite-scroll="load"
       class="list"
       :infinite-scroll-disabled="${disabled.value}"
+      :infinite-scroll-delay="${delay.value}"
+      :infinite-scroll-distance="${distance.value}"
     >
-      <li v-for="i in count1" :key="i" class="list-item">
+      <li v-for="i in count" :key="i" class="list-item">
         {{ i }}
       </li>
     </ul>
@@ -63,56 +49,31 @@ const disableSource = computed(() => {
     title="Data/InfiniteScroll"
     icon="carbon:auto-scroll"
   >
-    <Variant title="Basic usage" :source="basicSource">
-      <ul
-        v-infinite-scroll="load"
-        class="infinite-list"
-        style="overflow: auto"
-      >
-        <li v-for="i in count" :key="i" class="infinite-list-item">
-          {{ i }}
-        </li>
-      </ul>
-    </Variant>
-    <Variant title="Disable Loading" :source="disableSource">
+    <Variant title="Basic usage" :source="disableSource">
       <div class="infinite-list-wrapper" style="overflow: auto">
         <ul
-          v-infinite-scroll="load1"
+          v-if="refresh"
+          v-infinite-scroll="load"
           class="list"
           :infinite-scroll-disabled="disabled"
+          :infinite-scroll-delay="delay"
+          :infinite-scroll-distance="distance"
         >
-          <li v-for="i in count1" :key="i" class="list-item">
+          <li v-for="i in count" :key="i" class="list-item">
             {{ i }}
           </li>
         </ul>
       </div>
       <template #controls>
         <HstCheckbox v-model="disabled" title="infinite-scroll-disabled" />
+        <HstNumber v-model="delay" title="infinite-scroll-delay" />
+        <HstNumber v-model="distance" title="infinite-scroll-distance" />
       </template>
     </Variant>
   </Story>
 </template>
 
 <style scoped>
-.infinite-list {
-  height: 300px;
-  padding: 0;
-  margin: 0;
-  list-style: none;
-}
-.infinite-list .infinite-list-item {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 50px;
-  background: var(--el-color-primary-light-9);
-  margin: 10px;
-  color: var(--el-color-primary);
-}
-.infinite-list .infinite-list-item + .list-item {
-  margin-top: 10px;
-}
-
 .infinite-list-wrapper {
   height: 300px;
   text-align: center;
@@ -128,8 +89,8 @@ const disableSource = computed(() => {
   align-items: center;
   justify-content: center;
   height: 50px;
-  background: var(--el-color-danger-light-9);
-  color: var(--el-color-danger);
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
 }
 .infinite-list-wrapper .list-item + .list-item {
   margin-top: 10px;
