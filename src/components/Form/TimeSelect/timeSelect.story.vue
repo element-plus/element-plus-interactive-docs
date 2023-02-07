d20f8984dbbe4143cd926435bc95c3018ffc57e0
Du Qing <957965700@qq.com>
2023-02-07 10:57:42 +0800

    feat(TimeSelect): init TimeSelect (#110)

--- src/components/Form/TimeSelect/timeSelect.story.vue
+++ src/components/Form/TimeSelect/timeSelect.story.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { ISelectProps } from 'element-plus'
+import { Link } from '@element-plus/icons-vue'
 import sizeOptions from '@/constants/sizeOptions'
 
 // Basic Usage
