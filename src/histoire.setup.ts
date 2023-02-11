d8e756f1c9c42556f3207f76bfbac4839b0a1a32
flippedround <734243792@qq.com>
2023-02-11 10:41:41 +0800

    chore: update deps

--- src/histoire.setup.ts
+++ src/histoire.setup.ts
@@ -1,3 +1,13 @@
-import 'element-plus/theme-chalk/dark/css-vars.css'
 import './dark-mode'
 import './theme/style.css'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
+import { defineSetupVue3 } from '@histoire/plugin-vue'
+
+export const setupVue3 = defineSetupVue3(({ app }) => {
+  app.use(ElementPlus)
+})
+
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   resolve: {
@@ -30,14 +29,12 @@ export default defineConfig({
         './src/composables',
         './src/utils',
       ],
-      resolvers: [ElementPlusResolver()],
     }),
     Components({
       dirs: [
         'src/components',
         'src/controls-components',
       ],
-      resolvers: [ElementPlusResolver()],
     }),
 
   ],
