cac00ec04bba0bfca91b5ac5c55c7a0c861e25e2
allblueee <allblueee798@gmail.com>
2023-02-13 21:46:11 +0800

    fix(Drawer): lint:fix

--- src/components/Feedback/Drawer/drawer.story.vue
+++ src/components/Feedback/Drawer/drawer.story.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
+import { logEvent } from 'histoire/client'
 import type {
   basicDrawerProps,
   customizedDrawerProps,
   nestedDrawerProps,
-} from './constants';
-import { logEvent } from 'histoire/client';
-import { directionList } from './constants';
+} from './constants'
+import { directionList } from './constants'
 
 const openEvent = () => {
-  logEvent('open', { log: 'Drawer is opened' });
-};
+  logEvent('open', { log: 'Drawer is opened' })
+}
 
 const handleClose = (close: () => void) => {
-  logEvent('close', { log: 'Drawer is closed' });
-  close();
-};
+  logEvent('close', { log: 'Drawer is closed' })
+  close()
+}
 
 const basicDrawerData: basicDrawerProps = reactive({
   vModel: false,
@@ -30,7 +30,7 @@ const basicDrawerData: basicDrawerProps = reactive({
   size: '30%',
   title: 'Drawer',
   withHeader: true,
-});
+})
 
 const basicSource = computed(() => {
   return `<script setup lang="ts">
@@ -43,42 +43,42 @@ const basicSource = computed(() => {
 <el-drawer
   v-model="active"${isAttribute(
     !basicDrawerData.closeOnClickModal,
-    `close-on-click-modal=${basicDrawerData.closeOnClickModal}`
+    `close-on-click-modal=${basicDrawerData.closeOnClickModal}`,
   )}${isAttribute(
     !basicDrawerData.closeOnPressEscape,
-    `close-on-press-escape=${basicDrawerData.closeOnPressEscape}`
+    `close-on-press-escape=${basicDrawerData.closeOnPressEscape}`,
   )}${isAttribute(
     basicDrawerData.openDelay !== 0,
-    `open-delay=${basicDrawerData.openDelay}`
+    `open-delay=${basicDrawerData.openDelay}`,
   )}${isAttribute(
     basicDrawerData.closeDelay !== 0,
-    `close-delay=${basicDrawerData.closeDelay}`
+    `close-delay=${basicDrawerData.closeDelay}`,
   )}${isAttribute(
     basicDrawerData.distroyOnClose,
-    'distroy-on-close'
+    'distroy-on-close',
   )}${isAttribute(
     !basicDrawerData.modal,
-    `modal="${basicDrawerData.modal}"`
+    `modal="${basicDrawerData.modal}"`,
   )}${isAttribute(
     basicDrawerData.direction !== 'rtl',
-    `direction="${basicDrawerData.direction}"`
+    `direction="${basicDrawerData.direction}"`,
   )}${isAttribute(
     !basicDrawerData.showClose,
-    `show-close=${basicDrawerData.showClose}`
+    `show-close=${basicDrawerData.showClose}`,
   )}${isAttribute(
     basicDrawerData.size !== '30%',
-    `size="${basicDrawerData.size}"`
+    `size="${basicDrawerData.size}"`,
   )}${isAttribute(
     basicDrawerData.title !== '',
-    `title="${basicDrawerData.title}"`
+    `title="${basicDrawerData.title}"`,
   )}${isAttribute(
     !basicDrawerData.withHeader,
-    `with-header=${basicDrawerData.withHeader}`
+    `with-header=${basicDrawerData.withHeader}`,
   )}
 >
   <span>Hi, there!</span>
-</el-drawer>`;
-});
+</el-drawer>`
+})
 
 const customizedDrawerData: customizedDrawerProps = reactive({
   vModel: false,
@@ -96,7 +96,7 @@ const customizedDrawerData: customizedDrawerProps = reactive({
   withHeader: true,
   content: 'Customized content',
   footer: 'Customized footer',
-});
+})
 
 const customizedSource = computed(() => {
   return `<script setup lang="ts">
@@ -109,37 +109,37 @@ const customizedSource = computed(() => {
 <el-drawer
   v-model="active"${isAttribute(
     !customizedDrawerData.closeOnClickModal,
-    `close-on-click-modal=${customizedDrawerData.closeOnClickModal}`
+    `close-on-click-modal=${customizedDrawerData.closeOnClickModal}`,
   )}${isAttribute(
     !customizedDrawerData.closeOnPressEscape,
-    `close-on-press-escape=${customizedDrawerData.closeOnPressEscape}`
+    `close-on-press-escape=${customizedDrawerData.closeOnPressEscape}`,
   )}${isAttribute(
     customizedDrawerData.openDelay !== 0,
-    `open-delay=${customizedDrawerData.openDelay}`
+    `open-delay=${customizedDrawerData.openDelay}`,
   )}${isAttribute(
     customizedDrawerData.closeDelay !== 0,
-    `close-delay=${customizedDrawerData.closeDelay}`
+    `close-delay=${customizedDrawerData.closeDelay}`,
   )}${isAttribute(
     customizedDrawerData.distroyOnClose,
-    'distroy-on-close'
+    'distroy-on-close',
   )}${isAttribute(
     !customizedDrawerData.modal,
-    `modal="${customizedDrawerData.modal}"`
+    `modal="${customizedDrawerData.modal}"`,
   )}${isAttribute(
     customizedDrawerData.direction !== 'rtl',
-    `direction="${customizedDrawerData.direction}"`
+    `direction="${customizedDrawerData.direction}"`,
   )}${isAttribute(
     !customizedDrawerData.showClose,
-    `show-close=${customizedDrawerData.showClose}`
+    `show-close=${customizedDrawerData.showClose}`,
   )}${isAttribute(
     customizedDrawerData.size !== '30%',
-    `size="${customizedDrawerData.size}"`
+    `size="${customizedDrawerData.size}"`,
   )}${isAttribute(
     customizedDrawerData.title !== '',
-    `title="${customizedDrawerData.title}"`
+    `title="${customizedDrawerData.title}"`,
   )}${isAttribute(
     !customizedDrawerData.withHeader,
-    `with-header=${customizedDrawerData.withHeader}`
+    `with-header=${customizedDrawerData.withHeader}`,
   )}>
   <template #header>
     ${customizedDrawerData.title}
@@ -150,8 +150,8 @@ const customizedSource = computed(() => {
   <template #footer>
     ${customizedDrawerData.footer}
   </template>
-</el-drawer>`;
-});
+</el-drawer>`
+})
 
 const nestedDrawerData: nestedDrawerProps = reactive({
   vModel: false,
@@ -168,7 +168,7 @@ const nestedDrawerData: nestedDrawerProps = reactive({
   size: '30%',
   title: 'External drawer',
   withHeader: true,
-});
+})
 
 const nestedSource = computed(() => {
   return `<script setup lang="ts">
@@ -182,37 +182,37 @@ const nestedSource = computed(() => {
 <el-drawer
   v-model="externalActive"${isAttribute(
     !nestedDrawerData.closeOnClickModal,
-    `close-on-click-modal=${nestedDrawerData.closeOnClickModal}`
+    `close-on-click-modal=${nestedDrawerData.closeOnClickModal}`,
   )}${isAttribute(
     !nestedDrawerData.closeOnPressEscape,
-    `close-on-press-escape=${nestedDrawerData.closeOnPressEscape}`
+    `close-on-press-escape=${nestedDrawerData.closeOnPressEscape}`,
   )}${isAttribute(
     nestedDrawerData.openDelay !== 0,
-    `open-delay=${nestedDrawerData.openDelay}`
+    `open-delay=${nestedDrawerData.openDelay}`,
   )}${isAttribute(
     nestedDrawerData.closeDelay !== 0,
-    `close-delay=${nestedDrawerData.closeDelay}`
+    `close-delay=${nestedDrawerData.closeDelay}`,
   )}${isAttribute(
     nestedDrawerData.distroyOnClose,
-    'distroy-on-close'
+    'distroy-on-close',
   )}${isAttribute(
     !nestedDrawerData.modal,
-    `modal="${nestedDrawerData.modal}"`
+    `modal="${nestedDrawerData.modal}"`,
   )}${isAttribute(
     nestedDrawerData.direction !== 'rtl',
-    `direction="${nestedDrawerData.direction}"`
+    `direction="${nestedDrawerData.direction}"`,
   )}${isAttribute(
     !nestedDrawerData.showClose,
-    `show-close=${nestedDrawerData.showClose}`
+    `show-close=${nestedDrawerData.showClose}`,
   )}${isAttribute(
     nestedDrawerData.size !== '30%',
-    `size="${nestedDrawerData.size}"`
+    `size="${nestedDrawerData.size}"`,
   )}${isAttribute(
     nestedDrawerData.title !== '',
-    `title="${nestedDrawerData.title}"`
+    `title="${nestedDrawerData.title}"`,
   )}${isAttribute(
     !nestedDrawerData.withHeader,
-    `with-header=${nestedDrawerData.withHeader}`
+    `with-header=${nestedDrawerData.withHeader}`,
   )}>
   <template #default>
     <el-drawer      
@@ -230,26 +230,30 @@ const nestedSource = computed(() => {
       Open the internal Drawer
     </el-button>
   </template>
-</el-drawer>`;
-});
+</el-drawer>`
+})
 </script>
 
 <template>
   <Story title="Feedback/Drawer" icon="icomoon-free:drawer">
     <Variant title="Basic Usage" :source="basicSource">
-      <el-button @click="
-        basicDrawerData.vModel = true;
-      openEvent();
-      ">
+      <el-button
+        @click="
+          basicDrawerData.vModel = true;
+          openEvent();
+        "
+      >
         Open the drawer
       </el-button>
       <Teleport to="#app">
-        <el-drawer v-model="basicDrawerData.vModel" :close-on-click-modal="basicDrawerData.closeOnClickModal"
+        <el-drawer
+          v-model="basicDrawerData.vModel" :close-on-click-modal="basicDrawerData.closeOnClickModal"
           :close-on-press-escape="basicDrawerData.closeOnPressEscape" :open-delay="basicDrawerData.openDelay"
           :close-delay="basicDrawerData.closeDelay" :distroy-on-close="basicDrawerData.distroyOnClose"
           :modal="basicDrawerData.modal" :direction="basicDrawerData.direction" :show-close="basicDrawerData.showClose"
           :size="basicDrawerData.size" :title="basicDrawerData.title" :with-header="basicDrawerData.withHeader"
-          :before-close="handleClose">
+          :before-close="handleClose"
+        >
           <span>Hi, there!</span>
         </el-drawer>
       </Teleport>
@@ -268,19 +272,23 @@ const nestedSource = computed(() => {
       </template>
     </Variant>
     <Variant title="Customized Drawer" :source="customizedSource">
-      <el-button @click="
-        customizedDrawerData.vModel = true;
-      openEvent();
-      ">
+      <el-button
+        @click="
+          customizedDrawerData.vModel = true;
+          openEvent();
+        "
+      >
         Open a customized drawer
       </el-button>
       <Teleport to="#app">
-        <el-drawer v-model="customizedDrawerData.vModel" :close-on-click-modal="customizedDrawerData.closeOnClickModal"
+        <el-drawer
+          v-model="customizedDrawerData.vModel" :close-on-click-modal="customizedDrawerData.closeOnClickModal"
           :close-on-press-escape="customizedDrawerData.closeOnPressEscape" :open-delay="customizedDrawerData.openDelay"
           :close-delay="customizedDrawerData.closeDelay" :distroy-on-close="customizedDrawerData.distroyOnClose"
           :modal="customizedDrawerData.modal" :direction="customizedDrawerData.direction"
           :show-close="customizedDrawerData.showClose" :size="customizedDrawerData.size"
-          :with-header="customizedDrawerData.withHeader" :before-close="handleClose">
+          :with-header="customizedDrawerData.withHeader" :before-close="handleClose"
+        >
           <template #header>
             {{ customizedDrawerData.title }}
           </template>
@@ -300,8 +308,10 @@ const nestedSource = computed(() => {
         <HstNumber v-model="customizedDrawerData.closeDelay" title="Close-delay" />
         <HstCheckbox v-model="customizedDrawerData.distroyOnClose" title="Distroy-on-close" />
         <HstCheckbox v-model="customizedDrawerData.modal" title="Modal" />
-        <HstRadio v-model="customizedDrawerData.direction" title="Direction"
-          :options="customizedDrawerData.directionList" />
+        <HstRadio
+          v-model="customizedDrawerData.direction" title="Direction"
+          :options="customizedDrawerData.directionList"
+        />
         <HstCheckbox v-model="customizedDrawerData.showClose" title="Show-close" />
         <HstText v-model="customizedDrawerData.size" title="Size" />
         <HstText v-model="customizedDrawerData.title" title="Title" />
@@ -309,34 +319,44 @@ const nestedSource = computed(() => {
       </template>
     </Variant>
     <Variant title="Nested Drawer" :source="nestedSource">
-      <el-button @click="
-        nestedDrawerData.vModel = true;
-      openEvent();
-      ">
+      <el-button
+        @click="
+          nestedDrawerData.vModel = true;
+          openEvent();
+        "
+      >
         Open a nested drawer
       </el-button>
       <Teleport to="#app">
-        <el-drawer v-model="nestedDrawerData.vModel" :close-on-click-modal="nestedDrawerData.closeOnClickModal"
+        <el-drawer
+          v-model="nestedDrawerData.vModel" :close-on-click-modal="nestedDrawerData.closeOnClickModal"
           :close-on-press-escape="nestedDrawerData.closeOnPressEscape" :open-delay="nestedDrawerData.openDelay"
           :close-delay="nestedDrawerData.closeDelay" :distroy-on-close="nestedDrawerData.distroyOnClose"
           :modal="nestedDrawerData.modal" :direction="nestedDrawerData.direction"
           :show-close="nestedDrawerData.showClose" :size="nestedDrawerData.size" :title="nestedDrawerData.title"
-          :with-header="nestedDrawerData.withHeader" :before-close="handleClose">
+          :with-header="nestedDrawerData.withHeader" :before-close="handleClose"
+        >
           <template #default>
-            <el-drawer v-model="nestedDrawerData.internalVModel" size="20%" title="Internal drawer"
-              :append-to-body="true" :before-close="handleClose" />
+            <el-drawer
+              v-model="nestedDrawerData.internalVModel" size="20%" title="Internal drawer"
+              :append-to-body="true" :before-close="handleClose"
+            />
           </template>
           <template #footer>
-            <el-button @click="
-              nestedDrawerData.vModel = false;
-            openEvent();
-            ">
+            <el-button
+              @click="
+                nestedDrawerData.vModel = false;
+                openEvent();
+              "
+            >
               Cancel
             </el-button>
-            <el-button type="primary" @click="
-              nestedDrawerData.internalVModel = true;
-            openEvent();
-            ">
+            <el-button
+              type="primary" @click="
+                nestedDrawerData.internalVModel = true;
+                openEvent();
+              "
+            >
               Open the internal Drawer
             </el-button>
           </template>
