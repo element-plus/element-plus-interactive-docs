399084643c6b5050bb70e92fecf0c0cbb1876df5
acyza <101238421+acyza@users.noreply.github.com>
2023-02-17 09:51:21 +0800

    feat(MessageBox): init messagebox (#119)

---
+++ src/components/Feedback/MessageBox/controls.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+import { Edit, Warning } from '@element-plus/icons-vue'
+import { cloneDeep, isNil } from 'lodash'
+import { defaultMessageBoxOptions } from './controls'
+import type { MessageBoxOptions } from './controls'
+const props = defineProps<{
+  modelValue?: MessageBoxOptions
+}>()
+const emit = defineEmits(['update:modelValue'])
+const messageBoxData = ref<MessageBoxOptions>({} as any)
+watch([messageBoxData], () => {
+  const data = unref(messageBoxData)
+  if (data !== unref(props.modelValue))
+    emit('update:modelValue', data)
+})
+watch([props], () => {
+  if (props.modelValue && unref(messageBoxData) !== unref(props.modelValue))
+    messageBoxData.value = props.modelValue
+})
+onMounted(() => {
+  if (isNil(props.modelValue))
+    emit('update:modelValue', cloneDeep(defaultMessageBoxOptions))
+  else
+    messageBoxData.value = props.modelValue
+})
+</script>
+
+<template>
+  <HstText v-model="messageBoxData.title" title="title" />
+  <HstTextarea v-model="messageBoxData.message" title="message" />
+  <HstSelect
+    v-model="messageBoxData.type" title="type" :options="
+      [
+        {
+          label: 'undefined',
+          value: undefined,
+        },
+        'success',
+        'warning',
+        'info',
+        'error',
+      ]"
+  />
+  <HstSelect
+    v-model="messageBoxData.icon" title="icon" :options="[
+      {
+        label: 'undefined',
+        value: undefined,
+      },
+      {
+        label: 'Edit',
+        value: Edit,
+      },
+      {
+        label: 'Warning',
+        value: Warning,
+      },
+    ]"
+  />
+  <HstCheckbox v-model="messageBoxData.showClose" title="showClose" />
+  <HstCheckbox v-model="messageBoxData.center" title="center" />
+  <HstCheckbox v-model="messageBoxData.draggable" title="draggable" />
+</template>
---
+++ src/components/Feedback/MessageBox/messageBox.story.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { ElMessageBox } from 'element-plus'
+import { cloneDeep } from 'lodash'
+import { genSourceCode } from './gen-code'
+import controls, { defaultMessageBoxOptions } from './controls'
+import type { MessageBoxOptions } from './controls'
+
+// AlertMessageBox
+const alertMessageBoxOptions = ref<MessageBoxOptions>()
+const openAlertMessageBox = () => {
+  const options = alertMessageBoxOptions.value || cloneDeep(defaultMessageBoxOptions)
+  ElMessageBox.alert(options.title, options)
+}
+
+const alertMessageBoxSource = computed(() => {
+  return genSourceCode('alert', unref(alertMessageBoxOptions))
+})
+
+// ConfirmMessageBox
+const confirmMessageBoxOptions = ref<MessageBoxOptions>()
+const openConfirmMessageBox = () => {
+  const options = confirmMessageBoxOptions.value || cloneDeep(defaultMessageBoxOptions)
+  ElMessageBox.confirm(options.title, options)
+}
+
+const confirmMessageBoxSource = computed(() => {
+  return genSourceCode('confirm', unref(confirmMessageBoxOptions))
+})
+
+// PromptMessageBox
+const promptMessageBoxOptions = ref<MessageBoxOptions>()
+const openPromptMessageBox = () => {
+  const options = promptMessageBoxOptions.value || cloneDeep(defaultMessageBoxOptions)
+  ElMessageBox.prompt(options.title || 'title', options)
+}
+
+const promptMessageBoxSource = computed(() => {
+  return genSourceCode('prompt', unref(promptMessageBoxOptions))
+})
+
+// Customization
+const customizationMessageBoxOptions = ref<MessageBoxOptions>()
+
+const openCustomizationMessageBox = () => {
+  const options = customizationMessageBoxOptions.value || cloneDeep(defaultMessageBoxOptions)
+  ElMessageBox(options)
+}
+
+const customizationMessageBoxSource = computed(() => {
+  return genSourceCode(undefined, unref(customizationMessageBoxOptions))
+})
+</script>
+
+<template>
+  <Story title="Feedback/MessageBox" icon="ep:element-plus">
+    <Variant
+      title="Alert"
+      :source="alertMessageBoxSource"
+    >
+      <el-button @click="openAlertMessageBox">
+        Open the Alert Message Box
+      </el-button>
+      <template #controls>
+        <controls v-model="alertMessageBoxOptions" />
+      </template>
+    </Variant>
+    <Variant
+      title="Confirm"
+      :source="confirmMessageBoxSource"
+    >
+      <el-button @click="openConfirmMessageBox">
+        Open the Confirm Message Box
+      </el-button>
+      <template #controls>
+        <controls v-model="confirmMessageBoxOptions" />
+      </template>
+    </Variant>
+    <Variant
+      title="Prompt"
+      :source="promptMessageBoxSource"
+    >
+      <el-button @click="openPromptMessageBox">
+        Open the Prompt Message Box
+      </el-button>
+      <template #controls>
+        <controls v-model="promptMessageBoxOptions" />
+      </template>
+    </Variant>
+    <Variant
+      title="Customization"
+      :source="customizationMessageBoxSource"
+    >
+      <el-button @click="openCustomizationMessageBox">
+        Open the Customization Message Box
+      </el-button>
+      <template #controls>
+        <controls v-model="customizationMessageBoxOptions" />
+      </template>
+    </Variant>
+  </Story>
+</template>
+
+<style scoped>
+</style>
